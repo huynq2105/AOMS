426fbae0673a6bbc1673a51270bf09fae114fe1d
Nguyen Quang Huy <huynq.is@gmail.com>
2023-04-26 10:45:00 +0700

    fix loi permission ios, cau hinh thiet lap tham so chay tren ios

--- ios/AOMS/AppDelegate.m
+++ ios/AOMS/AppDelegate.m
@@ -3,7 +3,8 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
+#import <AppCenterReactNative.h>
+#import <CodePush/CodePush.h>
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -55,7 +56,8 @@ static void InitializeFlipper(UIApplication *application) {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+return [CodePush bundleURL];
+//return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
